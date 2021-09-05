75459fb9e037b885efd0279f37b18f4c04fdb8ea
mancomsci <90091779+mancomsci@users.noreply.github.com>
2021-09-05 09:42:12 +0700

    Upload Flutter layout

---
+++ layout/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:layout/pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "รับเขียนโปรแกรม ราคาหลักพัน",
+      home:HomePage(),
+    );
+  }
+
+  
+}
+
